23be3e759ee51d3e5bbe27603e6644950430b360
usaginotsuki <jcchachalo@gmail.com>
2020-08-30 22:59:44 -0500

    Revert "Before country select"
    This reverts commit b37bef394af9be82e092cbfb86ca81f3491294a3.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule, Routes } from '@angular/router';
 import { NgxPaginationModule } from 'ngx-pagination';
-import { MatSelectCountryModule } from '@angular-material-extensions/select-country';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -27,7 +26,6 @@ import { ComidaCardComponent } from './components/comida-main/comida-card/comida
 import { ComidaFormComponent } from './components/comida-main/comida-form/comida-form.component';
 import { HabitacionSearchComponent } from './components/habitacion-main/habitacion-search/habitacion-search.component';
 import { HabitacionCardComponent } from './components/habitacion-main/habitacion-card/habitacion-card.component';
-import { CuartoSearchComponent } from './components/cuarto-main/cuarto-search/cuarto-search.component';
 
 
 const routes:Routes=[
@@ -57,7 +55,6 @@ const routes:Routes=[
     ComidaFormComponent,
     HabitacionSearchComponent,
     HabitacionCardComponent,
-    CuartoSearchComponent,
   
   ],
   imports: [
@@ -67,8 +64,7 @@ const routes:Routes=[
     FontAwesomeModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
-    NgxPaginationModule,
-    MatSelectCountryModule
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
